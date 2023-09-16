e319871d20770e60926231708b19eba16af0c918
Adarsh Gupta <iamadarshgupta8@gmail.com>
2023-09-16 10:56:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# CosmicFlicks
+Movie Recommendation Web Application
+
+## Overview
+Dive into the world of personalized movie recommendations with CosmicFlicks. This project harnesses the power of the TMDB 5000 movies database to curate tailored movie suggestions just for you. Using an out-of-bag model for tag vectorization and the magic of cosine similarity, we bring you a simple yet effective movie recommendation system.
+
+## Project Structure
+- `Movie Recommendation System.ipynb`: The main Jupyter Notebook containing everything.
+- `tmdb_5000_credits.csv`: Dataset 1.
+- `tmdb_5000_movies.csv`: Dataset 2.
+- `main.py`: Streamlit Web Application.
+- 
+
+## Contributing
+
+Contributions to this project are welcome! Feel free to fork the repository, make improvements, and submit pull requests.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ main.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+movies_dict = pickle.load(open('movies.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarities = pickle.load(open('simi.pkl', 'rb'))
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=a3dbbe812adc0f5971b844cb4b163186&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarities[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key = lambda x:x[1])[1:6]
+    
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        recommended_movies_posters.append(fetch_poster(movies.iloc[i[0]].movie_id))
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies, recommended_movies_posters
+
+st.title("Movie Recommender System")
+
+selected_movie_name = st.selectbox('Select A Movie', movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters  = recommend(selected_movie_name)
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+    
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+        
